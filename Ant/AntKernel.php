440eed633a21cf0fdd56b0587bff87b132394fad
Amir Aslan Aslani <aa.aslani_6@yahoo.com>
2019-03-17 04:31:17 +0330

    public files sent to client

--- Ant/AntKernel.php
+++ Ant/AntKernel.php
@@ -43,10 +43,12 @@ class AntKernel {
     }
 
     private function handleRequest(Request $request) : Response {
+        global $_ANT;
         $method = $request->getMethod();
         $url = $request->getUri()->getPath();
+
+        // Detect Controller
         $match = $this->router->match($url, $method);
-        // \var_dump($match);
         if( is_array($match) && is_callable( $match['target'] ) ) {
             $response = $match['target']();
         	$response = $this->convertControllerResponseToServersOne(
@@ -54,8 +56,17 @@ class AntKernel {
             );
             return $response;
         }
-        echo 1;
 
+        // Detect Public File
+        $file = $_ANT['CONFIG']['paths']['public'] . $url;
+        if(file_exists($file)){
+            $type = $this->find_file_mime_type($file);
+            return new Response(Status::OK, [
+                "content-type" => "{$type}; charset=utf-8"
+            ], file_get_contents($file));
+        }
+
+        // Detect 404 Error!
         return new Response(Status::OK, [
             "content-type" => "text/plain; charset=utf-8"
         ], "404!");
@@ -63,6 +74,76 @@ class AntKernel {
 
     private function convertControllerResponseToServersOne($response){
         return $response;
+    }
+
+    private function find_file_mime_type($filename){
+        $mime_types = array(
+            'txt' => 'text/plain',
+            'htm' => 'text/html',
+            'html' => 'text/html',
+            'php' => 'text/html',
+            'css' => 'text/css',
+            'js' => 'application/javascript',
+            'json' => 'application/json',
+            'xml' => 'application/xml',
+            'swf' => 'application/x-shockwave-flash',
+            'flv' => 'video/x-flv',
+
+            // images
+            'png' => 'image/png',
+            'jpe' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'jpg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'bmp' => 'image/bmp',
+            'ico' => 'image/vnd.microsoft.icon',
+            'tiff' => 'image/tiff',
+            'tif' => 'image/tiff',
+            'svg' => 'image/svg+xml',
+            'svgz' => 'image/svg+xml',
+
+            // archives
+            'zip' => 'application/zip',
+            'rar' => 'application/x-rar-compressed',
+            'exe' => 'application/x-msdownload',
+            'msi' => 'application/x-msdownload',
+            'cab' => 'application/vnd.ms-cab-compressed',
+
+            // audio/video
+            'mp3' => 'audio/mpeg',
+            'qt' => 'video/quicktime',
+            'mov' => 'video/quicktime',
+
+            // adobe
+            'pdf' => 'application/pdf',
+            'psd' => 'image/vnd.adobe.photoshop',
+            'ai' => 'application/postscript',
+            'eps' => 'application/postscript',
+            'ps' => 'application/postscript',
+
+            // ms office
+            'doc' => 'application/msword',
+            'rtf' => 'application/rtf',
+            'xls' => 'application/vnd.ms-excel',
+            'ppt' => 'application/vnd.ms-powerpoint',
+
+            // open office
+            'odt' => 'application/vnd.oasis.opendocument.text',
+            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+        );
+
+        $value = explode(".", $filename);
+        $ext = strtolower(array_pop($value));
+        if (array_key_exists($ext, $mime_types)) {
+            return $mime_types[$ext];
+        } elseif (function_exists('finfo_open')) {
+            $finfo = finfo_open(FILEINFO_MIME);
+            $mimetype = finfo_file($finfo, $filename);
+            finfo_close($finfo);
+            return $mimetype;
+        } else {
+            return mime_content_type($filename);
+        }
     }
 
 }
